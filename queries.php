d2511487f6447e914242ad1217bd88b6bf15ae32
elizastuglik <elizastuglik6798@gmail.com>
2022-09-17 14:20:44 +0200

    D

--- queries.php
+++ queries.php
@@ -16,25 +16,10 @@
   }
  
   $query = '
-  PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
-  PREFIX owl: <http://www.w3.org/2002/07/owl#>
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-  PREFIX dc: <http://purl.org/dc/elements/1.1/>
-  PREFIX : <http://dbpedia.org/resource/>
-  PREFIX dbpedia2: <http://dbpedia.org/property/>
-  PREFIX dbpedia: <http://dbpedia.org/>
-  PREFIX dbpprop: <http://dbpedia.org/property/>
-
-  SELECT DISTINCT ?species ?binomial ?genus ?label
-  WHERE { ?species dbpedia-owl:family :Characidae;
-        dbpprop:genus ?genus;
-        rdfs:label ?label;
-        dbpedia2:binomial ?binomial.
-        filter ( langMatches(lang(?label), "en") ) }
-  ORDER BY ?genus';
+  PREFIX symbo:http://www.semanticweb.org/ontologies/SymbO# 
+  SELECT ?Line
+  WHERE {?Line symbo:express symbo:Musicality}
+  ';
   
   /* execute the query */
   $rows = $store->query($query, 'rows'); 
